73222f8ce4999151e2432a8c96b1f5e970ae3b0a
bblori <birton_lorand@yahoo.com>
2024-01-02 13:53:44 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Listing;
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Passing a lot of parameters to view page, which can seen in the front page.
 
+// Find all
+Route::get('/', [ListingController::class, 'index']);
+
+// Create listings
+Route::get('/listings/create', [ListingController::class,'create']);
+
+// Display Single post
+Route::get('/listings/{listing}', [ListingController::class,'show']);
